ac430cce8bdeee403e28fdf5b65435ff621d8abe
Yurii <yuriibielozertsev@gmail.com>
2018-10-30 14:46:09 +0200

    add sockets, use isFetching

--- src/containers/ChatPage.js
+++ src/containers/ChatPage.js
@@ -1,8 +1,9 @@
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux';
 import { logout } from '../actions/auth';
-import { fetchAllChats, fetchMyChats, setActiveChat, createChat, deleteChat, joinChat, leaveChat, sendMessage } from '../actions/chats'
+import { fetchAllChats, fetchMyChats, setActiveChat, createChat, deleteChat, joinChat, leaveChat } from '../actions/chats'
 import { editUser } from '../actions/users';
+import { sendMessage, mountChat, unmountChat, socketsConnect } from '../actions/sockets';
 import * as fromChats from '../reducers/chats';
 import * as fromState from '../reducers';
 import ChatPage from '../components/ChatPage';
@@ -38,6 +39,9 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   leaveChat,
   sendMessage,
   editUser,
+  mountChat,
+  unmountChat,
+  socketsConnect
 }, dispatch);
 
 
